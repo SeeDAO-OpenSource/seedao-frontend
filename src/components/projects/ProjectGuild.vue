fd20992f38b622150b4bf422f8c1bddf1ede3aba
FanCong <873602646@qq.com>
2022-04-28 02:21:54 +0800

    1、公会代码优化，点击公会跳转notion 2、开始创造跳转外链

--- src/components/projects/ProjectGuild.vue
+++ src/components/projects/ProjectGuild.vue
@@ -1,114 +1,56 @@
 <script>
+import GUILDS from "@/data/guilds.js";
 export default {
-	props: ['project'],
+    props: ["project"],
+    data() {
+        return {
+            guilds: GUILDS,
+        };
+    },
+    methods: {
+        handleJump(url) {
+            window.open(url);
+        },
+    },
 };
 </script>
 
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-4">
-    <div class="p-5">
-      <div class="flex flex-wrap justify-center content-center">
-        <div class="h-24 w-24 flex flex-wrap justify-center box-content backRound">
-          <img
-            src="@/assets/images/icon/TV.svg"
-            class="w-16"
-          />
+    <div class="grid grid-cols-1 md:grid-cols-4">
+        <div class="p-5" v-for="guildItem of guilds" :key="guildItem.id">
+            <div class="flex flex-wrap justify-center content-center">
+                <div
+                    class="h-24 w-24 flex flex-wrap justify-center box-content backRound"
+                >
+                    <img :src="guildItem.img" class="w-16" />
+                </div>
+            </div>
+            <p
+                class="pt-5 text-link"
+                @click.prevent="handleJump(guildItem.notion)"
+            >
+                {{ guildItem.name }}
+            </p>
         </div>
-      </div>
-      <p class="pt-5">开发者公会</p>
     </div>
-    <div class="p-5">
-      <div class="flex flex-wrap justify-center content-center">
-        <div class="h-24 w-24 flex flex-wrap justify-center box-content backRound">
-          <img
-            src="@/assets/images/icon/Camera.svg"
-            class="w-16"
-          />
-        </div>
-      </div>
-      <p class="pt-5">宣传公会</p>
-    </div>
-    <div class="p-5">
-      <div class="flex flex-wrap justify-center content-center">
-        <div class="h-24 w-24 flex flex-wrap justify-center box-content backRound">
-          <img
-            src="@/assets/images/icon/Remove_playlist.svg"
-            class="w-16"
-          />
-        </div>
-      </div>
-      <p class="pt-5">元宇宙建筑师公会</p>
-    </div>
-    <div class="p-5">
-      <div class="flex flex-wrap justify-center content-center">
-        <div class="h-24 w-24 flex flex-wrap justify-center box-content backRound">
-          <img
-            src="@/assets/images/icon/Knife.svg"
-            class="w-16"
-          />
-        </div>
-      </div>
-      <p class="pt-5">产品公会</p>
-    </div>
-    <div class="p-5">
-      <div class="flex flex-wrap justify-center content-center">
-        <div class="h-24 w-24 flex flex-wrap justify-center box-content backRound">
-          <img
-            src="@/assets/images/icon/Target.svg"
-            class="w-16"
-          />
-        </div>
-      </div>
-      <p class="pt-5">设计者公会</p>
-    </div>
-    <div class="p-5">
-      <div class="flex flex-wrap justify-center content-center">
-        <div class="h-24 w-24 flex flex-wrap justify-center box-content backRound">
-          <img
-            src="@/assets/images/icon/Messaging.svg"
-            class="w-20"
-          />
-        </div>
-      </div>
-      <p class="pt-5">运营公会</p>
-    </div>
-    <div class="p-5">
-      <div class="flex flex-wrap justify-center content-center">
-        <div class="h-24 w-24 flex flex-wrap justify-center box-content backRound">
-          <img
-            src="@/assets/images/icon/Files.svg"
-            class="w-16"
-          />
-        </div>
-      </div>
-      <p class="pt-5">翻译者公会</p>
-    </div>
-    <div class="p-5">
-      <div class="flex flex-wrap justify-center content-center">
-        <div class="h-24 w-24 flex flex-wrap justify-center box-content backRound">
-          <img
-            src="@/assets/images/icon/Router.svg"
-            class="w-16"
-          />
-        </div>
-      </div>
-      <p class="pt-5">投研公会</p>
-    </div>
-  </div>
 </template>
 
 <style lang="css" scoped>
-  .infoItem{
-		background: #FAFAFC;
-		border: 1px solid #000000;
-		box-sizing: border-box;
-		border-radius: 15px;
-	}
-	.backRound{
-		border-radius: 80px;
-		background: #FFFFFF;
-    border: 2px solid #5A5A5A;
+.infoItem {
+    background: #fafafc;
+    border: 1px solid #000000;
+    box-sizing: border-box;
+    border-radius: 15px;
+}
+.backRound {
+    border-radius: 80px;
+    background: #ffffff;
+    border: 2px solid #5a5a5a;
     box-sizing: border-box;
     box-shadow: 0px 3px 0px #626262;
-	}
+}
+.text-link:hover {
+    color: #0088ff;
+    text-decoration: underline;
+}
 </style>
---
+++ src/data/guilds.js
@@ -0,0 +1,50 @@
+const GUILDS = [
+    {
+        id: 1,
+        name: "开发者工会",
+        img: require("@/assets/images/icon/TV.svg"),
+        notion: "https://www.notion.so/98972f9683b74c419212c1e7ce53e725"
+    },
+    {
+        id: 2,
+        name: "宣传公会",
+        img: require("@/assets/images/icon/Camera.svg"),
+        notion: "https://www.notion.so/6ca8cea0977a4478b20ac0f2c5a2eec6"
+    },
+    {
+        id: 3,
+        name: "元宇宙建筑师公会",
+        img: require("@/assets/images/icon/Remove_playlist.svg"),
+        notion: "https://www.notion.so/4da6dceab15f42e8a17d6efe720da47d"
+    },
+    {
+        id: 4,
+        name: "产品公会",
+        img: require("@/assets/images/icon/Knife.svg"),
+        notion: "https://www.notion.so/b625d595e1f84776877e1a41ee7a6b9c"
+    },
+    {
+        id: 5,
+        name: "设计者公会",
+        img: require("@/assets/images/icon/Target.svg"),
+        notion: "https://www.notion.so/7e6e6f0a17d74b0ea17893c97ee2f534"
+    },
+    {
+        id: 6,
+        name: "运营公会",
+        img: require("@/assets/images/icon/Messaging.svg"),
+        notion: "https://www.notion.so/c6dfe75429f1428e90d89dd25b26269e"
+    },
+    {
+        id: 7,
+        name: "翻译者公会",
+        img: require("@/assets/images/icon/Files.svg"),
+        notion: "https://www.notion.so/d498b5e6919d4f2295bb76f80c83c4bf"
+    }, {
+        id: 8,
+        name: "投研公会",
+        img: require("@/assets/images/icon/Router.svg"),
+        notion: "https://www.notion.so/2600c97348cd415783b0af644c4765d3"
+    },
+]
+export default GUILDS;
