76983f1166e341fe0eca641661504e820418561d
fatfingererr <fatfingererr@gmail.com>
2022-04-28 14:24:42 +0800

    添加 wechat qrcode 弹窗在页面尾部图示

---
+++ src/components/shared/WechatQRCodeModal.vue
@@ -0,0 +1,113 @@
+<script>
+import feather from "feather-icons";
+
+export default {
+  props: ["showModal", "modal", "categories"],
+  data() {
+    return {};
+  },
+  mounted() {
+    feather.replace();
+  },
+  updated() {
+    feather.replace();
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <transition name="fade">
+    <div v-show="modal" class="font-general-regular fixed inset-0 z-30">
+      <!-- Modal body background as backdrop -->
+      <div
+        v-show="modal"
+        @click="showModal()"
+        class="
+          bg-filter bg-black bg-opacity-50
+          fixed
+          inset-0
+          w-full
+          h-full
+          z-20
+        "
+      ></div>
+      <!-- Modal content -->
+      <main class="flex flex-col items-center justify-center h-full w-full">
+        <transition name="fade-up-down">
+          <div v-show="modal" class="modal-wrapper flex items-center z-30">
+            <div
+              class="
+                modal
+                max-w-md
+                mx-5
+                xl:max-w-xl
+                lg:max-w-xl
+                md:max-w-xl
+                bg-secondary-light
+                dark:bg-primary-dark
+                max-h-screen
+                shadow-lg
+                flex-row
+                rounded-lg
+                relative
+              "
+            >
+              <div
+                class="modal-footer mt-2 sm:mt-0 py-5 px-8 border0-t text-center"
+              >
+			  <h2>扫码加入微信公众号</h2>
+                <img
+                  src="@/assets/images/wechat-qrcode.jpg"
+                  class="rounded-xl w-96"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </transition>
+      </main>
+    </div>
+  </transition>
+</template>
+
+<style scoped>
+.modal-body {
+  max-height: 500px;
+}
+.bg-gray-800-opacity {
+  background-color: #2d374850;
+}
+@media screen and (max-width: 768px) {
+  .modal-body {
+    max-height: 400px;
+  }
+}
+.fade-up-down-enter-active {
+  transition: all 0.3s ease;
+}
+.fade-up-down-leave-active {
+  transition: all 0.3s ease;
+}
+.fade-up-down-enter {
+  transform: translateY(10%);
+  opacity: 0;
+}
+.fade-up-down-leave-to {
+  transform: translateY(10%);
+  opacity: 0;
+}
+
+.fade-enter-active {
+  -webkit-transition: opacity 2s;
+  transition: opacity 0.3s;
+}
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
