f88fd9338afe7d5f397c2c1b032c50206ee4cfef
FanCong <873602646@qq.com>
2022-04-29 00:27:28 +0800

    代理测试2

--- src/services/discordbot.service.js
+++ src/services/discordbot.service.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 
 import { inject } from 'vue';
-import config from '@/data/config';
+// import config from '@/data/config';
 
 export default {
 
     init() {
         const axios = inject('axios');
-        if (axios !== undefined) {
-            if(process.env.NODE_ENV === 'production')
-            {
-                axios.defaults.baseURL = `${config.BACKEND_BOT_SERVER_URL}`;
-            }
-            else axios.defaults.baseURL = `${config.BACKEND_BOT_SERVER_URL}`;
-        }
+        // if (axios !== undefined) {
+        //     if(process.env.NODE_ENV === 'production')
+        //     {
+        //         axios.defaults.baseURL = `${config.BACKEND_BOT_SERVER_URL}`;
+        //     }
+        //     else axios.defaults.baseURL = `${config.BACKEND_BOT_SERVER_URL}`;
+        // }
         return axios;
     },
 
