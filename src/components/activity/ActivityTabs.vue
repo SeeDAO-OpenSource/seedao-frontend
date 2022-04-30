5d486200f5ebdcef2ba56bacd73e9b1de4fd5df5
qiwihui <qwh005007@gmail.com>
2022-04-30 17:52:39 +0800

    refactor: use v-bind style to reduce duplicate code

--- src/components/activity/ActivityTabs.vue
+++ src/components/activity/ActivityTabs.vue
@@ -23,36 +23,19 @@ export default {
 <template>
 	<div class="w-full pt-10 pb-10 flex flex-wrap justify-center content-center">
     <ul class="flex w-96 ul">
-      <li class="w-full" v-if="selected == '1'">
+      <li class="w-full">
           <a href="/activity"
             class="inline-block p-4 w-full text-gray-900 bg-gray-100
             rounded-l-lg focus:ring-4 focus:ring-blue-300"
-            style="background: #B7D2FF;border-radius: 16px;"
+            :style="[selected == '1' ? {'background': '#B7D2FF', 'border-radius': '16px'} : {'border-radius': '16px'}]"
             aria-current="page">
             活动
           </a>
       </li>
-      <li class="w-full" v-if="selected == '1'">
+      <li class="w-full">
           <a href="/bounty"
             class="inline-block p-4 w-full bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none"
-            style="border-radius: 16px;"
-          >
-            悬赏
-          </a>
-      </li>
-      <li class="w-full" v-if="selected == '2'">
-          <a href="/activity"
-            class="inline-block p-4 w-full text-gray-900 bg-gray-100
-            rounded-l-lg focus:ring-4 focus:ring-blue-300"
-            style="border-radius: 16px;"
-            aria-current="page">
-            活动
-          </a>
-      </li>
-      <li class="w-full" v-if="selected == '2'">
-          <a href="/bounty"
-            class="inline-block p-4 w-full bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none"
-            style="background: #B7D2FF;border-radius: 16px;"
+            :style="[selected == '2' ? {'background': '#B7D2FF', 'border-radius': '16px'} : {'border-radius': '16px'}]"
           >
             悬赏
           </a>
