03e7f4d463510c663ec197d1ebed9c11d058c412
0xRory <0x1rory@gmail.com>
2022-05-24 07:42:53 +0800

    [feat]-新增poap 发放画面

---
+++ src/views/PoapSend.vue
@@ -0,0 +1,29 @@
+<script>
+import PoapSend from "../components/integral/PoapSend.vue";
+
+export default {
+	components: {
+		PoapSend,
+	},
+	data: () => {
+		return {
+
+		};
+	},
+	mounted() {
+		//feather.replace();
+	},
+	updated() {
+		//feather.replace();
+	},
+	methods: {},
+};
+</script>
+
+<template>
+	<div
+		class="container mx-auto flex flex-col-reverse md:flex-row py-5"
+	>
+	<PoapSend />
+	</div>
+</template>
